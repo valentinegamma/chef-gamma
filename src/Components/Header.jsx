ef80b564f8ec992372a5ed3aecd3b5e18d654ffe
Valentine Gamma <gamma.dashboard@gmail.com>
2025-07-07 20:38:12 +0300

    V 5.1.0

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,10 @@
+import chefgamma from '../assets/Chef-Gamma.svg'
 function Header() {
   return(
     <>
       <header>
           <nav className="nav-list">
-            <img src="src/assets/Chef-Claude.svg" alt="Chef Claude" className="chef-gamma" />
+            <img src={chefgamma} alt="Chef Gamma" className="chef-gamma" />
             <span className="nav-heading">Chef Gamma</span>
           </nav>
       </header>
