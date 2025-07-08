ad80985cf61a0b9327af0cc60e2d044ed29c3b9c
Valentine Gamma <gamma.dashboard@gmail.com>
2025-07-08 09:13:21 +0300

    V 5.5.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
