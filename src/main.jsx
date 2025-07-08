e3070edb32db2df5225621cf2ed61dc596ee5d01
Valentine Gamma <gamma.dashboard@gmail.com>
2025-07-08 08:58:05 +0300

    V 5.4.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -8,5 +9,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
